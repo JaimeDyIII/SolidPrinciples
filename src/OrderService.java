490adc2b3367e1ec8286e84029db8b06b362bcea
jdy <dyiiijaime@gmail.com>
2025-03-29 18:49:39 +0800

    Finished

--- src/OrderService.java
+++ src/OrderService.java
@@ -1,22 +1,14 @@
 public class OrderService {
-    private Order order;
     private Calculable calculable;
     private Orderable orderable;
-    private InvoiceGenerator invoiceGenerator;
-    private NotificationSender notificationSender;
 
-    public OrderService(Order order, Calculable calculator, Orderable processor, InvoiceGenerator invoiceGenerator, NotificationSender notificationSender) {
-        this.order = order;
+    public OrderService(Calculable calculator, Orderable processor) {
         this.calculable = calculator;
         this.orderable = processor;
-        this.invoiceGenerator = invoiceGenerator;
-        this.notificationSender = notificationSender;
     }
 
-    public void processOrder(){
+    public void processOrder(Order order){
         orderable.placeOrder(order.getCustomerName(), order.getAddress());
         calculable.calculateTotal(order.getPrice(), order.getQuantity());
-        invoiceGenerator.generateInvoice("randomFileName.txt");
-        notificationSender.sendEmailNotification(order.getEmail());
     }
 }
--- src/OrderTest.java
+++ src/OrderTest.java
@@ -1,15 +1,18 @@
 public class OrderTest {
     public static void main(String[] args){
         Order order1 = new Order("Leo",
-                         "Unit 123, Something St. Malayo City",
-                                  12,
+                                 "Unit 123, Something St. Malayo City",
                                  540,
+                                 12,
                                  "Leopogi123@gmail.com");
         Orderable orderProcessor = new OrderProcessor();
         Calculable orderCalculator = new OrderCalculator();
         InvoiceGenerator orderInvoiceGenerator = new OrderInvoiceGenerator();
         NotificationSender emailNotificationSender = new EmailNotificationSender();
-        OrderService orderService = new OrderService(order1, orderCalculator, orderProcessor, orderInvoiceGenerator, emailNotificationSender);
-        orderService.processOrder();
+        OrderService orderService = new OrderService(orderCalculator, orderProcessor);
+        
+        orderService.processOrder(order1);
+        orderInvoiceGenerator.generateInvoice("order1.pdf");
+        emailNotificationSender.sendEmailNotification(order1.getEmail());
     }
 }
